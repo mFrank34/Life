9ab0ef67e53f2ea2f99337bbe8ed62ceef7bc662
Michael Frank <mf23acb@herts.ac.uk>
2025-10-29 22:58:03 +0000

    update

---
+++ build/CMakeFiles/Life.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/frank/Documents/vs_code_git/Life/Body/Cell.cpp" "CMakeFiles/Life.dir/Body/Cell.cpp.o" "gcc" "CMakeFiles/Life.dir/Body/Cell.cpp.o.d"
+  "/home/frank/Documents/vs_code_git/Life/Body/Chunk.cpp" "CMakeFiles/Life.dir/Body/Chunk.cpp.o" "gcc" "CMakeFiles/Life.dir/Body/Chunk.cpp.o.d"
+  "/home/frank/Documents/vs_code_git/Life/Body/Debug.cpp" "CMakeFiles/Life.dir/Body/Debug.cpp.o" "gcc" "CMakeFiles/Life.dir/Body/Debug.cpp.o.d"
+  "/home/frank/Documents/vs_code_git/Life/Body/Map.cpp" "CMakeFiles/Life.dir/Body/Map.cpp.o" "gcc" "CMakeFiles/Life.dir/Body/Map.cpp.o.d"
+  "/home/frank/Documents/vs_code_git/Life/Body/Neighbor.cpp" "CMakeFiles/Life.dir/Body/Neighbor.cpp.o" "gcc" "CMakeFiles/Life.dir/Body/Neighbor.cpp.o.d"
+  "/home/frank/Documents/vs_code_git/Life/Source/Main.cpp" "CMakeFiles/Life.dir/Source/Main.cpp.o" "gcc" "CMakeFiles/Life.dir/Source/Main.cpp.o.d"
+  "" "/home/frank/Documents/vs_code_git/Life/Bin/Life" "gcc" "CMakeFiles/Life.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Life.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "/home/frank/Documents/vs_code_git/Life/Bin/Life"
+  "/home/frank/Documents/vs_code_git/Life/Bin/Life.pdb"
+  "CMakeFiles/Life.dir/link.d"
+  "CMakeFiles/Life.dir/Body/Cell.cpp.o"
+  "CMakeFiles/Life.dir/Body/Cell.cpp.o.d"
+  "CMakeFiles/Life.dir/Body/Chunk.cpp.o"
+  "CMakeFiles/Life.dir/Body/Chunk.cpp.o.d"
+  "CMakeFiles/Life.dir/Body/Debug.cpp.o"
+  "CMakeFiles/Life.dir/Body/Debug.cpp.o.d"
+  "CMakeFiles/Life.dir/Body/Map.cpp.o"
+  "CMakeFiles/Life.dir/Body/Map.cpp.o.d"
+  "CMakeFiles/Life.dir/Body/Neighbor.cpp.o"
+  "CMakeFiles/Life.dir/Body/Neighbor.cpp.o.d"
+  "CMakeFiles/Life.dir/Source/Main.cpp.o"
+  "CMakeFiles/Life.dir/Source/Main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Life.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
