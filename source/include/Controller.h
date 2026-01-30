d9ac200f40ff368460fbf6db9acce20eac7848f8
Michael Frank <mffranks34@gmail.com>
2026-01-30 10:20:28 +0000

    adding to Controller

--- source/include/Controller.h
+++ source/include/Controller.h
@@ -1,4 +1,12 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
+#include <gtkmm/application.h>
+
+class Controller
+{
+public:
+    Controller()
+        : world(), manager(), ui(world);
+};
 
 #endif //CONTROLLER_H
