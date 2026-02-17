886be2b3fdc3c9b5a005c2ea492eefa4fba1209c
Michael Franks <mffranks34@gmail.com>
2026-02-17 21:31:07 +0000

    adding a pool threading system to create and manager the threads within the system... then planing on making a system to use this system

--- src/threading/Scheduler.cpp
+++ src/threading/Scheduler.cpp
@@ -1,34 +1,79 @@
 /*
- * File: Scheduler.cpp
+* File: Scheduler.cpp
  * Author: Michael Franks
- * Description:
+ * Description: Fixed thread-safe Scheduler implementation
  */
-
-// Scheduler.cpp
 
 #include "Scheduler.h"
 
 Scheduler::Scheduler(size_t num_threads)
-    : stop(false)
+    : stop(false), active_tasks(0), completed_tasks(0)
 {
+    for (size_t i = 0; i < num_threads; ++i)
+    {
+        workers.emplace_back([this, i] { worker(i); });
+    }
 }
 
 Scheduler::~Scheduler()
 {
+    {
+        std::unique_lock<std::mutex> lock(queue_mtx);
+        stop = true;
+    }
+
+    cv.notify_all(); // wake all workers to exit
+
+    for (auto& worker : workers)
+    {
+        if (worker.joinable())
+        {
+            worker.join();
+        }
+    }
 }
 
 int Scheduler::get_active_tasks() const
 {
+    return active_tasks.load();
 }
 
 int Scheduler::get_completed_tasks() const
 {
+    return completed_tasks.load();
 }
 
 int Scheduler::get_pending_tasks() const
 {
+    std::unique_lock<std::mutex> lock(queue_mtx);
+    return static_cast<int>(tasks.size());
 }
 
 void Scheduler::worker(int id)
 {
+    while (true)
+    {
+        std::function<void()> task;
+
+        {
+            std::unique_lock<std::mutex> lock(queue_mtx);
+            cv.wait(lock, [this] { return stop || !tasks.empty(); });
+
+            if (stop && tasks.empty())
+            {
+                break; // exit thread
+            }
+
+            task = std::move(tasks.front());
+            tasks.pop();
+        }
+
+        if (task)
+        {
+            active_tasks.fetch_add(1, std::memory_order_relaxed);
+            task();
+            active_tasks.fetch_sub(1, std::memory_order_relaxed);
+            completed_tasks.fetch_add(1, std::memory_order_relaxed);
+        }
+    }
 }
